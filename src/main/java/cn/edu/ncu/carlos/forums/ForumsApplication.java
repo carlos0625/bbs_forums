71da7274d5b59ad52ceb8a218cdcb2f71b8b36f8
carlos0625 <404561386@qq.com>
2018-12-12 00:11:28 +0800

    第一次提交，搭建后台spring boot框架，使用gradlem构建jpa+web+mysql

---
+++ src/main/java/cn/edu/ncu/carlos/forums/ForumsApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.ncu.carlos.forums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumsApplication.class, args);
+    }
+}
